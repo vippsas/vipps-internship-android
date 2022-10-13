4a193226ab490877e6b9865595be6be5c6372038
Hanne Lohne Try <hanne.lohne.try@vipps.no>
2022-10-13 14:55:43 +0200

    Initial commit

---
+++ app/src/main/java/no/vipps/summerinternship/MainActivity.kt
@@ -0,0 +1,18 @@
+package no.vipps.summerinternship
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import no.vipps.summerinternship.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/no/vipps/summerinternship/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package no.vipps.summerinternship.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
